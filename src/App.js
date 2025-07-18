789c7731390361f9473fc09aeecf20c12e28d1fa
bbky323 <bbky323@kau.kr>
2025-07-18 11:39:32 +0900

    Ngrok 테스트

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
-const CAMERA_API = "https://d2be351381b9.ngrok-free.app/video_feed"; // 카메라 서버 주소
+const CAMERA_API = "https://405e9c26a88f.ngrok-free.app"; // 카메라 서버 주소
 const AI_API = "https://petg.store"
 
 const Container = styled.div`
@@ -272,7 +272,7 @@ export default function PetCamUI() {
       <VideoSection>
         {streaming ? (
           <>
-            <VideoStream src="https://d2be351381b9.ngrok-free.app/video_feed" alt="Live" />
+            <VideoStream src="https://405e9c26a88f.ngrok-free.app/video_feed" alt="Live" />
             {mode === "train" && (
               <ResultBox>자세 결과: {poseResult || "분석 중..."}</ResultBox>
             )}
